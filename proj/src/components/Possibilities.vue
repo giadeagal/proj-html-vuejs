2125a30cc88495b7b66a99a20f60dfcda5810b58
Gianluca <gl.galassini@gmail.com>
2021-07-20 22:24:38 +0200

    sezione "possibilities"

--- proj/src/components/Possibilities.vue
+++ proj/src/components/Possibilities.vue
@@ -7,7 +7,18 @@
                     <li v-for="(obj, i) in sideList" :key="i" class="active text-capitalize">{{obj.text}}</li>
                 </ul>
               </div>
-              <div class="col"></div>
+              <div class="col-8">
+                  <h2 class="px-5 pt-5">Learning possibilities</h2>
+                  <p class="px-5 py-4">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatibus tenetur, pariatur deleniti quas minima ducimus cum, fugiat ex alias eaque distinctio aut porro? Fugit, asperiores dolorem autem deserunt magni perspiciatis culpa ea eaque repellendus placeat delectus tenetur, numquam omnis? Quam fuga error beatae aliquid laboriosam sit adipisci et molestiae ipsam!</p>
+                  <div class="center d-flex justify-content-between">
+                        <ul id="center-ul" class="">
+                            <li class="pb-2"><i class="fas fa-check"></i>Lorem ipsum dolor sit amet.</li>
+                            <li class="py-2"><i class="fas fa-check"></i>Lorem ipsum dolor sit amet.</li>
+                            <li class="py-2"><i class="fas fa-check"></i>Lorem ipsum dolor sit amet.</li>
+                        </ul>
+                    <img :src="img" alt="#" class="align-self-end">
+                  </div>
+              </div>
           </div>
       </div>
   </section>
@@ -18,6 +29,7 @@ export default {
     name: "possibilities",
     data() {
         return {
+            img: require("@/img/h12-tabs-icon-1.png"),
             sideList:[
                 {
                     text:"what we do",
@@ -53,7 +65,7 @@ export default {
 @import "@/styles/vars.scss";
 
 #possibilities {
-    height: 800px;
+    height: 600px;
     border-bottom: 1px solid lightgray;
 }
 
@@ -64,13 +76,46 @@ export default {
         padding: 20px;
         text-transform: uppercase;
         font-weight: 500;
+        font-size: 1.3rem;
 
         &:hover {
             border-left: 10px solid #40c4ff; 
             cursor: pointer;
-            color: $myTBlue1;
+            color: $myTBlue2;
             background-color: $myBGGray1;
         }
     }
+}
+
+p {
+    color: $myTGray4;
+    font-size: 1.3rem;
+}
+
+#center-ul {
+    width: 50%;
+    padding-left: 50px;
+
+    li {
+        display: block;
+        text-transform: initial;
+        font-size: 1.3rem;
+        color: $myTGray4;
+
+        i {
+            margin-right: 15px;
+            color: $myTBlue1;
+
+            &:hover {
+                cursor: initial;
+            }
+        }
+    }
+}
+
+img {
+    margin-right: 50px;
+    width: 150px;
+    min-height: 150px;
 }
 </style>
